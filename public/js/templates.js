d7ece70b528afe0bfb3b0a2cc2ae823aad68258c
Daniel Saewitz <switz213@gmail.com>
2013-06-18 13:32:14 -0400

    Some template and css improvements

--- public/js/templates.js
+++ public/js/templates.js
@@ -12,13 +12,13 @@ function program1(depth0,data) {
   data.buffer.push("<button>View All</button>");
   }
 
-  data.buffer.push("<h3>Submit a new standup</h3>\n<form ");
+  data.buffer.push("<div class=new-standup>\n  <h3>Submit a new standup</h3>\n  <form ");
   hashContexts = {'on': depth0};
   hashTypes = {'on': "STRING"};
   data.buffer.push(escapeExpression(helpers.action.call(depth0, "submit", {hash:{
     'on': ("submit")
   },contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
-  data.buffer.push(">\n  <p>\n    <label for=status>Status</label>\n    ");
+  data.buffer.push(">\n    <p>\n      <label for=status>Status</label>\n      ");
   hashContexts = {'contentBinding': depth0,'valueBinding': depth0,'name': depth0,'id': depth0};
   hashTypes = {'contentBinding': "STRING",'valueBinding': "STRING",'name': "ID",'id': "ID"};
   data.buffer.push(escapeExpression(helpers.view.call(depth0, "Ember.Select", {hash:{
@@ -27,7 +27,7 @@ function program1(depth0,data) {
     'name': ("status"),
     'id': ("status")
   },contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
-  data.buffer.push("\n  </p>\n  <p>\n    <label for=today>Today</label>\n    ");
+  data.buffer.push("\n    </p>\n    <p>\n      <label for=today>Today</label>\n      ");
   hashContexts = {'value': depth0,'id': depth0};
   hashTypes = {'value': "ID",'id': "ID"};
   options = {hash:{
@@ -35,7 +35,7 @@ function program1(depth0,data) {
     'id': ("today")
   },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
   data.buffer.push(escapeExpression(((stack1 = helpers.textarea),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "textarea", options))));
-  data.buffer.push("\n  </p>\n  <p>\n    <label for=tomorrow>Tomorrow</label>\n    ");
+  data.buffer.push("\n    </p>\n    <p>\n      <label for=tomorrow>Tomorrow</label>\n      ");
   hashContexts = {'value': depth0,'id': depth0};
   hashTypes = {'value': "ID",'id': "ID"};
   options = {hash:{
@@ -43,7 +43,7 @@ function program1(depth0,data) {
     'id': ("tomorrow")
   },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
   data.buffer.push(escapeExpression(((stack1 = helpers.textarea),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "textarea", options))));
-  data.buffer.push("\n  </p>\n  <p>\n    <label for=questions>Questions</label>\n    ");
+  data.buffer.push("\n    </p>\n    <p>\n      <label for=questions>Questions/Issues</label>\n      ");
   hashContexts = {'value': depth0,'id': depth0};
   hashTypes = {'value': "ID",'id': "ID"};
   options = {hash:{
@@ -51,7 +51,7 @@ function program1(depth0,data) {
     'id': ("questions")
   },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
   data.buffer.push(escapeExpression(((stack1 = helpers.textarea),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "textarea", options))));
-  data.buffer.push("\n  <p>\n    <label for=user>User</label>\n    ");
+  data.buffer.push("\n    <p>\n      <label for=user>User</label>\n      ");
   hashContexts = {'value': depth0,'type': depth0,'id': depth0};
   hashTypes = {'value': "ID",'type': "STRING",'id': "ID"};
   options = {hash:{
@@ -60,13 +60,13 @@ function program1(depth0,data) {
     'id': ("user")
   },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
   data.buffer.push(escapeExpression(((stack1 = helpers.input),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "input", options))));
-  data.buffer.push("\n  </p>\n  <input type=submit />\n  ");
+  data.buffer.push("\n    </p>\n    <input type=submit />\n    ");
   hashTypes = {};
   hashContexts = {};
   options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
   stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "updates", options) : helperMissing.call(depth0, "linkTo", "updates", options));
   if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
-  data.buffer.push("\n</form>\n");
+  data.buffer.push("\n  </form>\n</div>");
   return buffer;
   
 });
@@ -126,7 +126,7 @@ function program4(depth0,data) {
   options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
   stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "index", options) : helperMissing.call(depth0, "linkTo", "index", options));
   if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
-  data.buffer.push("</p>\n\n<table border=1 cellpadding=5 width=100%>\n  <tr>\n    <th>Status</th>\n    <th>Today</th>\n    <th>Tomorrow</th>\n    <th>Questions</th>\n    <th>User</th>\n  </tr>\n  ");
+  data.buffer.push("</p>\n\n<table border=1 cellpadding=5 width=90%>\n  <tr>\n    <th>Status</th>\n    <th>Today</th>\n    <th>Tomorrow</th>\n    <th>Questions/Issues</th>\n    <th>User</th>\n  </tr>\n  ");
   hashTypes = {};
   hashContexts = {};
   stack2 = helpers.each.call(depth0, "model", {hash:{},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
